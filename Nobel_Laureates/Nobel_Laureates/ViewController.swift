306ee32ca9f9d4c5dbf0630e717cd7263c6f3f87
Shyam Ji Mishra <shyamji.mishra@citrix.com>
2020-12-12 12:22:37 +0530

    Initial commit

---
+++ Nobel_Laureates/Nobel_Laureates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nobel_Laureates
+//
+//  Created by Shyam on 12/12/20.
+//  Copyright © 2020 shyamOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Nobel_Laureates/Nobel_LaureatesTests/Nobel_LaureatesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Nobel_LaureatesTests.swift
+//  Nobel_LaureatesTests
+//
+//  Created by Shyam on 12/12/20.
+//  Copyright © 2020 shyamOrg. All rights reserved.
+//
+
+import XCTest
+@testable import Nobel_Laureates
+
+class Nobel_LaureatesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
